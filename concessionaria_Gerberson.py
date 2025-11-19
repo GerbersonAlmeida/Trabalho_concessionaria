2cb9c34480939a619e77d380322b65dfb52b3dcb
Gerberson Almeida <gerberson.dev@gmail.com>
2025-11-19 18:38:44 -0300

    criando a primeira funcao

--- concessionaria_Gerberson.py
+++ concessionaria_Gerberson.py
@@ -1,9 +1,11 @@
 #Codigo para iniciar o atendimento:
     #codigo para fazer cadastro, necessario criar um objeto para guardar os dados cadstrados
-print("################ Bem vindo ao sistema! ###################")
-cliente = input("Digite o nome do cliente: ")
-cpf = input("Digite o cpf do usuário: ")
-
+#função para criar linha
+def criar_linha():
+    print(128 * "_")
+#cliente = input("Digite o nome do cliente: ")
+#cpf = input("Digite o cpf do usuário: ")
+criar_linha()
 
     #Codigo para escolher o tipo de transação
 
