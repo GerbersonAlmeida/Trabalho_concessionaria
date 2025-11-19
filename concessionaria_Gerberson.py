c45fc4f8f28ce04b47a357664f1bcb133a8c7c27
Gerberson Almeida <gerberson.dev@gmail.com>
2025-11-18 21:42:32 -0300

    Primeiro commit do projeto

---
+++ concessionaria_Gerberson.py
@@ -0,0 +1,13 @@
+#Codigo para iniciar o atendimento:
+    #codigo para fazer cadastro, necessario criar um objeto para guardar os dados cadstrados
+print("################ Bem vindo ao sistema! ###################")
+cliente = input("Digite o nome do cliente: ")
+cpf = input("Digite o cpf do usuário: ")
+
+
+    #Codigo para escolher o tipo de transação
+
+
+#Codigo para mostrar os modelos disponiveis para compra
+#Código para mostrar os modelos disponivies para aluguel 
+#Codigo para mostrar o valor pelo qual será recebido o veiculo usado na troca ou na venda. 
