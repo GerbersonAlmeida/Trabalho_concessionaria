1e290fe07fec2d90b2df1b3e8a180d0a5f5eaf99
Gerberson Almeida <gerberson.dev@gmail.com>
2025-11-26 21:46:07 -0300

    ta dando erro

--- concessionaria_Gerberson.py
+++ concessionaria_Gerberson.py
@@ -11,56 +11,36 @@ def menu():
     print(f" {30 * "="} {"ESCOLHA QUAL OPERÇÃO VOCÊ DESEJA APLICAR:\n"} {30 * "="}")
     print(f" {30 * " "} {"\n1 - COMPRAR\n2 - VENDER\n3 - ALUGAR\n 0 - SAIR\n"} {30 * "="}")
 
-
+#Função usada para que o usuario nao digite seus dados errados
 def cadastro_cliente():
     while True:
-        cadastro['nome'] = input("Digite o nome do cliente: ").title()
+        cadastro['nome'] = input("Digite o nome do cliente: ").title().strip()
         while True:
-            cadastro['Telefone'] = input("Número para contato com 9 digitos: ").strip()
-            cadastro['saldo'] = input("Informe o saldo disponivel para negociar: ")
-            valor_telefone = cadastro["Telefone"]
-            if  valor_telefone.isdigit and len(valor_telefone) == 9:
-                break
+            cadastro['Telefone'] = input("Número para contato com DDD e 9 digitos: ").strip()
+            print() #print vazio sendo utilizado apenas para pular linha
+            
+            if cadastro['Telefone'].isdigit() and len(cadastro['Telefone']) == 11: #if utilizado para conferir se se trata de apenas números para ser valido como telefone
+                break                                                   #Um len() foi utilizado para ver se o número digitado se tem a quantidade correta de números
+            elif cadastro['Telefone'].isdigit() and len(cadastro['Telefone']) != 11:
+                print("O número de telefone digitado parece estar faltando digitos, \ninforme o número de elefone com DDD")
+                print()
             else:
                 print("O valor digitado não é um número de telefone, informe o número de telefone")
+                print()
+        while True:    
+            cadastro['saldo'] = input("Informe o saldo disponivel para negociar R$: ")
+            if cadastro['saldo'].isdigit():
+                saldo = cadastro['saldo']
+                cadastro['saldo'] :  float(saldo)
+                print(cadastro['saldo'])
+                break
+            else:
+                print("Valor digitado não parece ser um saldo,\n por favor digite o saldo disponivel para continuarmos!")
+                print()
 
-    
-    
-# Dicionario vazio criado para guardar temporariamente o cadastro do cliente
-cadastro = {
-
-}
-
-mostrar_titulo("CADASTRO DE CLIENTES")
-
-#Codigo criado para adicionar as chaves e valores no dicionario cadastro = {}
-while True:
-    cadastro['nome'] = input("Digite o nome do cliente: ").title()
-    cadastro['Telefone'] = input("Número para contato: ")
-    cadastro['saldo'] = input("Informe o saldo disponivel para negociar: ")
-
-    #estrutura de repetição criada para mostrar os dados cadastrado
-    mostrar_titulo("CONFIRA O CADASTRO")
-    print(cadastro["Telefone"])
- #   for k, v in cadastro.items():
-'''   print(f"{k} = {v}")
-    decisao = input("Os dados informados estâo corretos: [S/N] ").capitalize()
-    if decisao == "S":
-        mostrar_linha()
-        print(" " * 30, "DADOS CADASTRADOS COM SUCESSO!!\n")
-
-        #Mostrando os dados finais do cliente
-        for k, v in cadastro.items():
-            print(f"{" " * 30} {k} = {v}")
         break
-    #Se incorreto os dados esse else vai deletar os dados guardados no dicionario cadastro
-    else:
-        print("Digite novamente os dados do cliente!")
-        del cadastro["Nome"]
-        del cadastro["Saldo"]
-        del cadastro["Telefone"]
-        mostrar_linha()
-#Lista para guardar os dicionarios dos carros contendo: marca, modelo e km
+
+    
 carros = [{
           'id' :'001',# id 001 serão carros a venda 
           'Marca' : "Chevrolet",
@@ -74,7 +54,41 @@ carros = [{
           'Modelo' : 'Onix',
           'km' : 10000
         }]
+# Dicionario vazio criado para guardar temporariamente o cadastro do cliente
+cadastro = {
 
+}
+
+mostrar_titulo("CADASTRO DE CLIENTES")
+
+#Codigo criado para adicionar as chaves e valores no dicionario cadastro = {}
+while True:
+    cadastro_cliente()
+    #estrutura de repetição criada para mostrar os dados cadastrado
+    
+    for k, v in cadastro.items():
+        print(f"{k} = {v}")
+        decisao = input("Os dados informados estâo corretos: [S/N] ").capitalize()
+        if decisao == "S":
+            mostrar_linha()
+            print(" " * 30, "DADOS CADASTRADOS COM SUCESSO!!\n")
+
+            #Mostrando os dados finais do cliente
+            for k, v in cadastro.items():
+                print(f"{" " * 30} {k} = {v}")
+            break
+        #Se incorreto os dados esse else vai deletar os dados guardados no dicionario cadastro
+        else:
+            print("Digite novamente os dados do cliente!")
+            del cadastro["Nome"]
+            del cadastro["Saldo"]
+            del cadastro["Telefone"]
+            mostrar_linha()
+
+    break
+#Lista para guardar os dicionarios dos carros contendo: marca, modelo e km
+        
+'''
 mostrar_linha()
 #Codigo para escolher o tipo de transação
 mostrar_titulo(" MENÚ DE TRANSAÇÃO ")
